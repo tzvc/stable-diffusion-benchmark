ebfb462052c167d38350534ab43a18f200e670eb
theochampion <contact.theochampion@gmail.com>
2022-11-24 19:44:08 +0100

    --gpus=all

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -eu
 
 CWD=$(basename "$PWD")
 
-docker run --rm --gpus-all \
+docker run --rm --gpus=all \
     -v huggingface:/home/huggingface/.cache/huggingface \
     -v "$PWD"/input:/home/huggingface/input \
     -v "$PWD"/output:/home/huggingface/output \
