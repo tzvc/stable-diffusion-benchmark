b1ec293c583f7d8103cc20960884ad1ccd6a9023
theochampion <contact.theochampion@gmail.com>
2022-11-24 19:45:35 +0100

    Use $1 in run script

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ docker run --rm --gpus=all \
     -v huggingface:/home/huggingface/.cache/huggingface \
     -v "$PWD"/input:/home/huggingface/input \
     -v "$PWD"/output:/home/huggingface/output \
-    -it "$CWD"
+    -it $1
