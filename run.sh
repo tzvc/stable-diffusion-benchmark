4fd1a0ba58fe7ed1d19613e3567b5c11635339c7
theochampion <contact.theochampion@gmail.com>
2022-11-24 19:50:36 +0100

    HUGGINGFACE_TOKEN Env

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ docker run --rm --gpus=all \
     -v huggingface:/home/huggingface/.cache/huggingface \
     -v "$PWD"/input:/home/huggingface/input \
     -v "$PWD"/output:/home/huggingface/output \
+    -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN \
     -it $1
